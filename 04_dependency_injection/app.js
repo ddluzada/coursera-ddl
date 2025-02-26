f7b7133003578a332d1a73f1904352c51e7ebe73
dennis luzada <dennis.luzada@gmail.com>
2025-02-26 13:25:30 +0800

    Dependency injection and minification.

---
+++ 04_dependency_injection/app.js
@@ -0,0 +1,20 @@
+// (function() {
+//     'use strict';
+    
+//     angular
+//         .module('DIApp', [])
+//         .controller('DIController', DIController);
+
+//     DIController.$inject = ['$scope', '$filter'];
+
+//     function DIController($scope, $filter) {
+//         $scope.name = 'Dennis';
+
+//         $scope.upper = function() {
+//             var upCase = $filter('uppercase');
+//             $scope.name = upCase($scope.name);
+//         };
+//     }
+// })();
+
+!function(){"use strict";function e(e,n){e.name="Dennis",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
